aaf036ca78c9e31449a69607fb5addd7ff49475b
bradshac <bradshac@rose-hulman.edu>
2017-12-06 01:31:11 -0500

    finished m7

--- src/m8_loops_and_objects.py
+++ src/m8_loops_and_objects.py
@@ -19,6 +19,8 @@ def main():
     draw_circles1()
     print_sequence2()
     draw_circles2()
+    print_sequence3()
+    draw_circles3()
 
 
 def print_sequence1():
@@ -112,7 +114,7 @@ def draw_circles2():
     -- Waits for the user to press the mouse, then closes the window.
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement this function, per its doc-string above.
+    # Done: 5. Implement this function, per its doc-string above.
     # Put a statement in  main  to test this function.
     # REQUIREMENT: You must use a   RANGE  statement to solve this problem.
     # ------------------------------------------------------------------
@@ -152,6 +154,10 @@ def print_sequence3():
     print('Running print_sequence3:')
     print('--------------------------------------------------')
 
+    print('Prints:')
+    for k in range(100):
+        print(k+1)
+
 
 def draw_circles3():
     """
@@ -170,6 +176,15 @@ def draw_circles3():
     print('--------------------------------------------------')
     print('Running draw_circles3:  See graphics window')
     print('--------------------------------------------------')
+
+    window = rg.RoseWindow(300, 300)
+
+    for k in range(100):
+        circle = rg.Circle(rg.Point(200, 150), k+1)
+        circle.attach_to(window)
+        window.render(.05)
+
+    window.close_on_mouse_click()
 
 
 def print_cosines():
