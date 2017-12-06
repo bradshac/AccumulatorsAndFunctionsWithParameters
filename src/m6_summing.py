34195057e300346c9c189300ed8b7f50e914f114
bradshac <bradshac@rose-hulman.edu>
2017-12-06 00:09:40 -0500

    finished m3

--- src/m6_summing.py
+++ src/m6_summing.py
@@ -4,9 +4,9 @@ in its simplest classic forms:
    SUMMING:       total = total + number
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
-
+         their colleagues and Alexander Bradshaw.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
+import math
 
 def main():
     """ Calls the   TEST   functions in this module. """
@@ -29,8 +29,28 @@ def run_test_sum_cosines():
     print('Testing the   sum_cosines   function:')
     print('--------------------------------------------------')
 
+    expected = .13416
+    answer = sum_cosines(4)
+    print('Test 1 expected:', expected)
+    print('       actual:  ', answer)
+
+    expected = .72435
+    answer = sum_cosines(7)
+    print('Test 1 expected:', expected)
+    print('       actual:  ', answer)
+
+    expected = 1.33827
+    answer = sum_cosines(14)
+    print('Test 1 expected:', expected)
+    print('       actual:  ', answer)
+
 
 def sum_cosines(n):
+    total = 0
+    for k in range(n):
+        total = total + math.cos(k)
+    return total
+
     """
     What comes in:  A non-negative integer n.
     What goes out:  The sum of the cosines of the integers
@@ -65,8 +85,18 @@ def run_test_sum_square_roots():
     print('Testing the   sum_square_roots   function:')
     print('--------------------------------------------------')
 
+    expected = 11.854408
+    answer = sum_square_roots(5)
+    print('Test 1 expected:', expected)
+    print('       actual:  ', answer)
 
+    
 def sum_square_roots(n):
+    total = 0
+    for k in range(n+1):
+        total = total + math.sqrt(2*k)
+    return total
+
     """
     What comes in:  A non-negative integer n.
     What goes out:  The sum of the square roots of the integers
